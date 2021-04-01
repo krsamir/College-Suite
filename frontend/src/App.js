2bdf41c6e39b84e25fd578a8b9c5cfc86cd814c7
samir <samir1741012321@gmail.com>
2021-04-01 20:24:41 +0530

    Integration of api with frontend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,18 @@
+import React,{useState} from 'react'
 import "./App.css";
-
+import axios from "axios";
 function App() {
-  return <div className="App">
-    <h2>College Suite</h2>
-  </div>;
+  const [apiData, setApiData] = useState([])
+  axios
+    .get("/api/test")
+    .then((response) => setApiData(response.data))
+    .catch((error) => console.log(error));
+  return (
+    <div className="App">
+      <h2>College Suite</h2>
+      {JSON.stringify(apiData)}
+    </div>
+  );
 }
 
 export default App;
